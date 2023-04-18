1f04c6d90efe3950ba69bfa5ea3a9845151d5571
abooday sy <ghostsy123321@gmail.com>
2023-04-18 16:10:30 +0200

    'returnopdarchKLAAR'

---
+++ Returnfucntions/Returnfucntions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+
+namespace Returnfucntions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            Console.WriteLine(program.geTallen());
+            int whole = program.geTallen();
+            Console.WriteLine(whole);
+        }
+
+
+        private int geTallen()
+        {
+            int num1 = 20;
+            int num2 = 40;
+
+            int sum = num1 + num2;
+            return sum;
+
+        }
+
+    }
+}
